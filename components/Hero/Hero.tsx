1b2b78a8ccb09cb862ed9aef22e7880ee886750e
stormsidali2001 <assoulsidali@gmail.com>
2022-08-03 18:48:40 +0200

    svg separator path in hero section  done

--- components/Hero/Hero.tsx
+++ components/Hero/Hero.tsx
@@ -2,7 +2,15 @@ import styles from './Hero.module.css'
 const Hero = () => {
   return (
     <div className={styles.container}>
+        <div className={styles.left}></div>
+        <div className={styles.center}>
+        <svg width="184" height="978" viewBox="0 0 184 978" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M170 14L14 378.155L135.47 431.767L14 978" stroke="#90C8AC" strokeWidth="27" strokeLinecap="round" strokeLinejoin="round"/>
+        <text  x='25%' y='70%' >VS</text>
+        </svg>
 
+        </div>
+        <div className={styles.right}></div>
     </div>
   )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
+import Hero from '../components/Hero/Hero'
 import Topbar from '../components/Topbar/Topbar'
 import styles from '../styles/Home.module.css'
 
@@ -10,6 +11,7 @@ const Home: NextPage = () => {
 
       <Topbar/>
       <main className={styles.main}>
+        <Hero/>
       </main>
 
     </div>

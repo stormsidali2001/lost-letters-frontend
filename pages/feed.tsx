df9c6b386ce210c13fab18f5d2525b8242e8b0db
stormsidali2001 <assoulsidali@gmail.com>
2022-08-04 11:12:13 +0200

    ...

--- pages/feed.tsx
+++ pages/feed.tsx
@@ -1,4 +1,7 @@
 import { NextPage } from "next"
+import Leftbar from "../components/Leftbar/Leftbar";
+import Posts from "../components/posts/Posts";
+import Rightbar from "../components/Rightbar/Rightbar";
 import TopbarUser from "../components/TopbarUser/TopbarUser";
 import styles from '../styles/Feed.module.css';
 const Feed:NextPage = () => {

4479b0dd168bb12dddd8dbb72fccf90e8b769400
stormsidali2001 <assoulsidali@gmail.com>
2022-08-04 11:45:41 +0200

    adding some svg icons from figma

--- components/Leftbar/Leftbar.tsx
+++ components/Leftbar/Leftbar.tsx
@@ -1,8 +1,40 @@
+import FriendsIcon from '../icons/FriendsIcon'
+import HammerIcon from '../icons/HammerIcon'
+import HitoryIcon from '../icons/HitoryIcon'
+import HomeIcon from '../icons/HomeIcon'
+import InvitationIcon from '../icons/InvitationIcon'
 import styles from './Leftbar.module.css'
 const Leftbar = () => {
+  const links = [
+    {
+        text:'Home',
+        icon:<HomeIcon/>
+    },
+    {
+        text:'Friends',
+        icon:<FriendsIcon/>
+    },
+    {
+        text:'Invitations',
+        icon:<InvitationIcon/>
+    },
+    {
+        text:'Play',
+        icon:<HammerIcon/>
+    },
+    {
+        text:'History',
+        icon:<HitoryIcon/>
+    },
+
+  ]
   return (
     <div className={styles.container}>
-
+        <ul className={styles.links}>
+            <li className={styles.link}>
+                
+            </li>
+        </ul>
     </div>
   )
 }

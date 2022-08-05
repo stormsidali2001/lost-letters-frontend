8fef97db6b2d2341b7e9cf3823c8daa9614251f5
stormsidali2001 <assoulsidali@gmail.com>
2022-08-05 20:39:06 +0200

    some fake data for the cards from figma

--- components/FriendsSuggestions/FriendsSuggestions.tsx
+++ components/FriendsSuggestions/FriendsSuggestions.tsx
@@ -1,5 +1,48 @@
 import styles from './FriendSuggestions.module.css';
 const FriendsSuggestions = () => {
+  const friendsSuggestions = [
+    {
+      fullname:'Julian Wan',
+      profilImgUrl:'Julian Wan.jpg',
+      description:"descriptive text about me this text is supposed to introduce my self brievely"
+    },
+    {
+      fullname:'Ludovic Migneault',
+      profilImgUrl:'Ludovic Migneault.jpg',
+      description:"descriptive text about me this text is supposed to introduce my self brievely"
+    },
+    {
+      fullname:'Kier In Sight',
+      profilImgUrl:'Kier In Sight.jpg',
+      description:"descriptive text about me this text is supposed to introduce my self brievely"
+    },
+    {
+      fullname:'Vicky Hladynets',
+      profilImgUrl:'Vicky Hladynets.jpg',
+      description:"descriptive text about me this text is supposed to introduce my self brievely"
+    },
+    {
+      fullname:'Albert Dera',
+      profilImgUrl:'Albert Dera.jpg',
+      description:"descriptive text about me this text is supposed to introduce my self brievely"
+    },
+    {
+      fullname:'ian dooley',
+      profilImgUrl:'ian dooley.jpg',
+      description:"descriptive text about me this text is supposed to introduce my self brievely"
+    },
+    {
+      fullname:'Darshan Patel',
+      profilImgUrl:'Darshan Patel.jpg',
+      description:"descriptive text about me this text is supposed to introduce my self brievely"
+    },
+    {
+      fullname:'Petr Sevcovic',
+      profilImgUrl:'Petr Sevcovic',
+      description:"descriptive text about me this text is supposed to introduce my self brievely"
+    }
+
+  ]
   return (
     <div className={styles.container}>
 

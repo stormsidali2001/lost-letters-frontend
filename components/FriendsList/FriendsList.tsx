8117d5197224c99e5bfa2c7a911c91a45426cb5c
stormsidali2001 <assoulsidali@gmail.com>
2022-08-05 14:08:49 +0200

    FriendsList component ....

---
+++ components/FriendsList/FriendsList.tsx
@@ -0,0 +1,10 @@
+import styles from './Friendslist.module.css';
+const FriendsList = () => {
+  return (
+    <div className={styles.container}>
+        
+    </div>
+  )
+}
+
+export default FriendsList

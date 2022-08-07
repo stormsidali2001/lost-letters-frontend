c5cb96774294cb9b2a38bd5caf39d0f545ffb173
stormsidali2001 <assoulsidali@gmail.com>
2022-08-07 11:48:01 +0200

    setting up the game page interface

---
+++ components/GamePlayerArea/GamePlayerArea.tsx
@@ -0,0 +1,10 @@
+import styles from './GamePlayerArea.module.css';
+const GamePlayerArea = () => {
+  return (
+    <div className={styles.container}>
+
+    </div>
+  )
+}
+
+export default GamePlayerArea
--- pages/game.tsx
+++ pages/game.tsx
@@ -1,6 +1,41 @@
+import GamePlayerArea from '../components/GamePlayerArea/GamePlayerArea';
+import TopbarUser from '../components/TopbarUser/TopbarUser';
+import styles from '../styles/Game.module.css';
+
+
 const Game = () => {
+    const oponent = {
+            fullname:'Reza Biazar',
+            level:'32',
+            hp:0.8,
+            exp:0.5,
+            word:'sidali',
+            description:"think of a savage animal who run at high spped"
+        };
+    const player = {
+        fullname:'Assoul Sidali',
+        level:'32',
+        hp:0.8,
+        exp:0.5,
+        word:'sidali',
+        description:"think of a savage animal who run at high spped"
+    };
+    
   return (
-    <div>game</div>
+    <div className={styles.container}>
+        <TopbarUser/>
+        <main className={styles.main}>
+            <div className={styles.wrapper}>
+                <div className={styles.top}></div>
+                <div className={styles.bottom}>
+                    <div className={styles.leftCanvasContainer}></div>
+                    <GamePlayerArea/>
+                    <div className={styles.rightCanvasContainer}></div>
+                </div>
+            </div>
+        </main>
+
+    </div>
   )
 }
 

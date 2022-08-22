39c3109f2d8bdb7c51b8cf53e2c086e4f79c7d75
stormsidali2001 <assoulsidali@gmail.com>
2022-08-22 15:58:19 +0200

    ....

--- pages/game.tsx
+++ pages/game.tsx
@@ -27,6 +27,9 @@ const Game = () => {
         if(floor) res = Math.floor(res);
         return res;
     }
+    function dist(obj1:{x:number,y:number},obj2:{x:number,y:number}){
+        return Math.hypot(obj1.x-obj2.x,obj1.y-obj2.y)
+    }
     function generateCircles(numCircles:number,descW:number,descH:number) {
         const active:{x:number,y:number}[] = []
        
@@ -34,22 +37,64 @@ const Game = () => {
         const k = 30;
         const w = r / Math.sqrt(2);
         
-        const rows = Math.floor((descW-70)/w);
-        const cols = Math.floor((descH-70)/w);
+        const rows = Math.floor((descW)/w);
+        const cols = Math.floor((descH)/w);
         const grid:({x:number,y:number} |undefined)[] = []
         for(let i = 0;i<rows*cols;i++){
             grid[i] = undefined;
         }
-        //chosing a random point in the grid
+        //chosing a random point in the grid and push it to the active list
         let x = descW/2
         let y = descH/2
         const col = Math.floor(y/w);
         const row =  Math.floor(x/w);
         const X0 = {x,y};
-
-      
         grid[row*cols +col] = X0;
         active.push(X0);
+        while(active.length >0){
+            const randomIndex = Math.floor(Math.random()*active.length);
+            const point = active[randomIndex];
+            let found = false;
+            for(let t = 0;t<k;t++){
+                //trying k times to find points far from the choosen active point by [r,2r] with a random angle
+                //the points should be choosen uniformly
+                let randomRadius = random(r,2*r);
+                let randomAngle = random(0,Math.PI*2);
+
+                const sample = {
+                    x:point.x + randomRadius*Math.cos(randomAngle),
+                    y:point.y - randomRadius*Math.sin(randomAngle)
+                };
+                let col =  Math.floor(sample.x/w)
+                let row = Math.floor(sample.y/w)
+                //checking if one neighbor at list exists in a distance <r
+                if (col > -3 && row > -3 && col < cols && row < rows && !grid[row*cols +col]) {
+                        var neighborExist = false;
+                        for (var i = -3; i <= 3; i++) {
+                            for (var j = -3; j <= 3; j++) {
+                                    var neighbor = grid[(row+j)*cols +col+i];
+                                    if (neighbor) {
+                                        var d = dist(sample, neighbor);
+                                        if (d < r) {
+                                            neighborExist = true;
+                                        }
+                                    }
+                            }
+                        }
+                        if (!neighborExist) {
+                            found = true;
+                            grid[row*cols +col] = sample
+                         
+                            active.push(sample);
+                            break;
+                          }
+               
+                }
+            
+        
+                
+            }
+        }
       }
   return (
     <div className={styles.container}>
